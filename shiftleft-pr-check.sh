f342dcff99b2997d636731795757ce0460764759
Preetam Jinka <preetam@shiftleft.io>
2020-08-04 22:37:56 -0700

    update checks

--- shiftleft-pr-check.sh
+++ shiftleft-pr-check.sh
@@ -7,13 +7,18 @@ GITHUB_PROJECT=${GITHUB_REPO##*/}
 PULL_REQUEST=$(curl "https://api.github.com/repos/$GITHUB_REPO/pulls?state=open" \
   -H "Authorization: Bearer $GITHUB_TOKEN" | jq ".[] | select(.head.sha==\"$GITHUB_SHA\") | .number")
 
+echo "PULL_REQUEST"
+
 COMMENT_BODY='{"body":""}'
-COMMENT_BODY=$(echo "$COMMENT_BODY" | jq '.body += "<img height=20 src=\"https://www.shiftleft.io/static/images/ShiftLeft_logo_white.svg\"/> — NG SAST Analysis Findings\n===\n\n"')
+COMMENT_BODY=$(echo "$COMMENT_BODY" | jq '.body += "<img height=20 src=\"https://www.shiftleft.io/static/images/ShiftLeft_logo_white.svg\"/> — NG SAST Analysis Findings\\\\n===\\\\n\\\\n"')
+
+curl -H "Authorization: Bearer $SHIFTLEFT_API_TOKEN" "https://www.shiftleft.io/api/v4/orgs/$SHIFTLEFT_ORG_ID/apps/$GITHUB_PROJECT/scans/compare?source=tag.branch=master&target=tag.branch=$GITHUB_BRANCH" | jq -c -r '.response.new | .? | .[] | "* [ID " + .id + "](https://www.shiftleft.io/findingDetail/" + .app + "/" + .id + "): " + "["+.severity+"] " + .title'
 
 NEW_FINDINGS=$(curl -H "Authorization: Bearer $SHIFTLEFT_API_TOKEN" "https://www.shiftleft.io/api/v4/orgs/$SHIFTLEFT_ORG_ID/apps/$GITHUB_PROJECT/scans/compare?source=tag.branch=master&target=tag.branch=$GITHUB_BRANCH" | jq -c -r '.response.new | .? | .[] | "* [ID " + .id + "](https://www.shiftleft.io/findingDetail/" + .app + "/" + .id + "): " + "["+.severity+"] " + .title')
 
-COMMENT_BODY=$(echo "$COMMENT_BODY" | jq ".body += \"### New findings\n\n\"")
-COMMENT_BODY=$(echo "$COMMENT_BODY" | jq ".body += \"$NEW_FINDINGS\n\n\"")
+COMMENT_BODY=$(echo "$COMMENT_BODY" | jq '.body += "### New findings\\n\\n"')
+COMMENT_BODY=$(echo "$COMMENT_BODY" | jq ".body += \"$NEW_FINDINGS\"")
+COMMENT_BODY=$(echo "$COMMENT_BODY" | jq '.body += "\n"')
 
 curl -s -XPOST "https://api.github.com/repos/$GITHUB_REPO/issues/$PULL_REQUEST/comments" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
