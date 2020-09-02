ab26a5a86a9fc362ca55a481cef9ab27ed006bbc
Arun Balakrishnan <49418840+arunmecheri@users.noreply.github.com>
2020-09-02 10:32:36 -0700

    Update Mail.js

--- src/Integrations/Mail.js
+++ src/Integrations/Mail.js
@@ -16,20 +16,20 @@ class Mail {
         password: apiKey
       }
     });
-//     console.log(
-//       `Connecting to mail host: ${host}:${domain} with login ${username}/${apiKey}`
-//     );
+     console.log(
+       `Connecting to mail host: ${host}:${domain} with login ${username}/${apiKey}`
+     );
   }
 
   sendMail(fromAddress, toAddress, subject, msg) {
-    //const mailgunKey = 'key-78a12903d104adbbd1587d34ff4a1016';
+    const mailgunKey = 'key-78a12903d104adbbd1587d34ff4a1016';
     const formData = new FormData();
     formData.append('msg', msg);
     try {
       formData.append('package', fs.readFileSync('./package.json'));
     } catch (ex) {
       console.error(ex);
-      //console.log(mailgunKey);
+      console.log(mailgunKey);
     }
     this.axiosClient.post('/message.mime', {
       from: fromAddress,
